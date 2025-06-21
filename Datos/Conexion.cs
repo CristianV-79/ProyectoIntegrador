c5d30de3c122f040c5fbcfdd261bff170241ba92
CristianV-79 <ecvivar@outlook.com>
2025-06-21 00:30:03 -0300

    Ingresar datos de conexion MySQL por teclado

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -20,11 +20,42 @@ namespace ProyectoIntegrador.Datos
         //Asignamos valores a las variables de conexion
         private Conexion()
         {
+            //Variables usadas para la repeticion de lineas de codgo
+            bool correcto = false;
+            int mensaje;
+            //Creamos las variables para recibir los datos desde el teclado
+            string T_servidor = "Servidor";
+            string T_puerto = "Puerto";
+            string T_usuario = "Usuario";
+            string T_clave = "Clave";
+            //Se antepone la T para indicar que vienen desde teclado
+            /*Usamos un ciclo While para volver a repetir el ingreso de datos
+             la variable "correcto" la inicializamos para ingresar al ciclo*/
+            while (correcto != true)
+            {
+                //Armamos los cuadros de dialogo para el ingreso de datos
+                T_servidor = Microsoft.VisualBasic.Interaction.InputBox("Ingrese Servidor", "DATOS DE INSTALACION MySQL");
+                T_puerto = Microsoft.VisualBasic.Interaction.InputBox("Ingrese Puerto", "DATOS DE INSTALACION MySQL");
+                T_usuario = Microsoft.VisualBasic.Interaction.InputBox("Ingrese Usuario", "DATOS DE INSTALACION MySQL");
+                T_clave = Microsoft.VisualBasic.Interaction.InputBox("Ingrese Clave", "DATOS DE INSTALACION MySQL");
+                /*Controlamos que los datos ingresados para acceder a MySQL son correctos*/
+                mensaje = (int)MessageBox.Show("Su ingreso: SERVIDOR = " + T_servidor + " - PUERTO = " + T_puerto + " - USUARIO: " + T_usuario + " - CLAVE: " + T_clave, "AVISO DEL SISTEMA", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (mensaje != 6) //El valor corresponde a si
+                {
+                    MessageBox.Show("INGRESE NUEVAMENTE LOS DATOS");
+                    correcto = false;
+                }
+                else
+                {
+                    correcto = true;
+                }
+            }
+            /*Reemplazamos los datos concretos que teniamos por las variables*/
             this.baseDatos = "proyecto";
-            this.servidor = "localhost";
-            this.puerto = "3306";
-            this.usuario = "root";
-            this.clave = "8462";
+            this.servidor = T_servidor;
+            this.puerto = T_puerto;
+            this.usuario = T_usuario;
+            this.clave = T_clave;
         }
         //Proceso de interaccion
         public MySqlConnection CrearConexion()
